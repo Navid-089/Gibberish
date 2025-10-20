2bc927c0abc8e0c62894969aa98b3a2b4f30168b
Wahid Al Azad Navid <wahidnavid@gmail.com>
2025-10-21 02:08:25 +0600

    21 oct

---
+++ printf_behavior/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int secret = 42;
+    /* should print 42 if protection isn't used*/
+    printf("The secret number is: %d\n");
+}
+
+// command to compile and run: 
