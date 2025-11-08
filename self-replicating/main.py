18a5bf988df3079db812825f8f95f8e923f6a329
Wahid Al Azad Navid <wahidnavid@gmail.com>
2025-11-08 13:40:59 +0600

    hmm

--- self-replicating/main.py
+++ self-replicating/main.py
@@ -1,5 +1,10 @@
 key = 74 
 encrypted = [58, 56, 35, 36, 62, 98, 104, 2, 47, 38, 38, 37, 102, 106, 61, 37, 56, 38, 46, 107, 104, 99]
+# Printing encrypted in ascii 
+
+printable = ''.join(chr(c) if 32 <= c <= 126 else '.' for c in encrypted)
+print(printable)
+
 decoded = ''.join(chr(c^key) for c in encrypted)
 # print(decoded)
 exec(decoded)
